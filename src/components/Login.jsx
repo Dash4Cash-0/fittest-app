309deaaca2c3f24e9f8292eebbd478830cb846db
Kim Zugic <kim.zugic@live.com>
2025-04-29 21:35:55 +0200

    fixes to login screen

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,10 +9,12 @@ function Login(){
       <div className="login-card">
         <h2>Log in to your account</h2>
         <div className="user-input">
-          <p>Username:</p>
-          <p>Password:</p>
-          <input type="text" className="user-input"/>
-          <input type="password" className="password-input"/>
+          
+            <p>Username:</p>
+            <input type="text" className="username-input"/>
+            <p>Password:</p>
+            <input type="password" className="password-input"/>
+          
           <div className="login-register">
             <button type="submit" className="login-btn">Login</button>
             <button type="submit" className="register-btn">Register</button>
